3502ee653128ee998b2319813e6d3a8bf55d5cc4
chris <chris@wearecmyk.com>
2026-02-13 15:14:16 -0500

    Initial Commit

---
+++ PhoneTag3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PhoneTag3
+//
+//  Created by chris langer on 2/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhoneTag3/PhoneTag3App.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneTag3App.swift
+//  PhoneTag3
+//
+//  Created by chris langer on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PhoneTag3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
