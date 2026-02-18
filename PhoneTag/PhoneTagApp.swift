09f4c0d5750e02c3f0e65cafdd6f3dd822993fde
chris <chris@wearecmyk.com>
2026-02-18 12:27:32 -0500

    FIXED THE LOGIN

--- PhoneTag/PhoneTagApp.swift
+++ PhoneTag/PhoneTagApp.swift
@@ -1,15 +1,49 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
 
 @main
 struct PhoneTagApp: App {
-    init() {
-        FirebaseApp.configure()
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        // Only needed for Phone Auth + APNs (safe to leave in)
+        application.registerForRemoteNotifications()
+
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        if Auth.auth().canHandleNotification(userInfo) {
+            completionHandler(.noData)
+            return
+        }
+        completionHandler(.newData)
+    }
+
+    // Optional but commonly needed for Phone Auth on iOS
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Auth.auth().setAPNSToken(deviceToken, type: .unknown) // use .sandbox/.prod if you want
     }
 }
